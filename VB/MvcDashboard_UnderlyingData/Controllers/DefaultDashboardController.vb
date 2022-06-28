ad2e0be30478e95dfc5f82250bbcefd6d7bb104d
DevExpressExampleBot <fakeEmail>
2022-06-28 16:52:50 +0400

    Source auto update [skip ci]

---
+++ VB/MvcDashboard_UnderlyingData/Controllers/DefaultDashboardController.vb
@@ -0,0 +1,9 @@
+Imports DevExpress.DashboardWeb.Mvc
+
+Namespace MvcDashboard_UnderlyingData.Controllers
+
+    Public Class DefaultDashboardController
+        Inherits DashboardController
+
+    End Class
+End Namespace
